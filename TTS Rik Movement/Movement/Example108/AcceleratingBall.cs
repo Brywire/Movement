4e2725086615c50d907e48a6c8839a494975dd5f
Albert Buitenhuis <ba.buitenhuis@gmail.com>
2023-03-21 11:57:59 +0100

    Opdracht Accelerating Ball

--- TTS Rik Movement/Movement/Example108/AcceleratingBall.cs
+++ TTS Rik Movement/Movement/Example108/AcceleratingBall.cs
@@ -1,4 +1,5 @@
 using System.Numerics; // Vector2
+using System;
 using Raylib_cs; // Color
 
 /*
@@ -22,8 +23,10 @@ namespace Movement
 {
 	class AcceleratingBall : SpriteNode
 	{
-		// your private fields here (add Velocity, Acceleration, and MaxSpeed)
-
+		// your private fields here (MaxSpeed)
+		Vector2 Velocity = new Vector2();
+		Vector2 Acceleration = new Vector2();
+		float maxspeed = 1000;
 
 		// constructor + call base constructor
 		public AcceleratingBall() : base("resources/ball.png")
@@ -31,22 +34,36 @@ namespace Movement
 			Position = new Vector2(Settings.ScreenSize.X / 2, Settings.ScreenSize.Y / 4);
 			Color = Color.RED;
 		}
-
+			private Vector2 Limit(Vector2 vec2, float max)
+			{
+				Vector2 limited = vec2;
+				if (limited.Length() > max)
+				{
+					limited = Vector2.Normalize(limited);
+					limited *= max;
+				}
+				return limited;
+			}
 		// Update is called every frame
 		public override void Update(float deltaTime)
 		{
+			Acceleration = new Vector2(40, 30);
+			Velocity = Limit(Velocity, maxspeed);
 			Move(deltaTime);
 			WrapEdges();
+			// Check to see what the max speed is
+			//Console.WriteLine(Velocity.Length());
 		}
 
 		// your own private methods
 		private void Move(float deltaTime)
 		{
 			// TODO implement
-			// Position += Velocity * deltaTime;
+			Velocity += Acceleration * deltaTime;
+			Position += Velocity * deltaTime;
 
-			// accelerate your ball (40, 30) every frame
 			// limit to a maximum speed of 1000 pixels/second
+			Acceleration *= 0;
 		}
 
 		private void WrapEdges()
@@ -59,8 +76,25 @@ namespace Movement
 			// TODO implement...
 			if (Position.X > scr_width)
 			{
-				// ...
+				Position.X = 0;
 			}
+
+			if (Position.X < 0)
+			{
+				Position.X = scr_width;
+			}
+
+			if (Position.Y > scr_height)
+			{
+				Position.Y = 0;
+			}
+
+			if (Position.Y < 0)
+			{
+				Position.Y = scr_height;
+			}
+
+
 		}
 
 	}
